10c9bd3a777f20bb99ce9dc3a3cb297bde9a9885
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-04-11 23:55:36 -0700

    Check test failed

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -16,7 +16,7 @@ public class Test {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some dat"));
 
 
     }
